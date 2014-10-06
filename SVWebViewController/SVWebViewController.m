5997793e10f677a2bb33224e7204c4c2991449e9
Nissan Tsafrir <nissan.tsafrir@gmail.com>
2014-10-07 02:21:20 +0300

    Show the toolbar after the view did appear to handle a case when container view frame is chagned

--- SVWebViewController/SVWebViewController.m
+++ SVWebViewController/SVWebViewController.m
@@ -174,7 +174,10 @@
     NSAssert(self.navigationController, @"SVWebViewController needs to be contained in a UINavigationController. If you are presenting SVWebViewController modally, use SVModalWebViewController instead.");
     
 	[super viewWillAppear:animated];
-	
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
         [self.navigationController setToolbarHidden:NO animated:animated];
     }
